7bbb547ee51686e8ad6f9045c1203453e12bbc61
Dirk Pranke <dpranke@google.com>
2021-11-02 16:33:21 -0700

    Add scripts that will export the content for the site.
    This copies over the scripts from /experimental/website
    that export the content from Google Sites and will
    enable us to actually check in the markdown content.
    
    However, the first pass of that will be in a follow-on CL.
    
    Bug: 1260171
    Change-Id: Ia257cbbfde089385ca11a391eb4779a0b9bed0ee
    Reviewed-on: https://chromium-review.googlesource.com/c/website/+/3258094
    Reviewed-by: Struan Shrimpton <sshrimp@google.com>
    Commit-Queue: Dirk Pranke <dpranke@google.com>

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -7,11 +7,46 @@
 See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details about the presubmit API built into depot_tools.
 """
+
+import os
+
+
 PRESUBMIT_VERSION = '2.0.0'
 
 # This line is 'magic' in that git-cl looks for it to decide whether to
 # use Python3 instead of Python2 when running the code in this file.
 USE_PYTHON3 = True
+
+
+# This list must be kept in sync with the lists in //.eleventy.js and
+# //scripts/upload_lobs.py.
+# TODO(dpranke): Figure out how to share these lists to eliminate the
+# duplication and need to keep them in sync.
+
+LOB_EXTENSIONS = [
+                  '.ai',
+                  '.bin',
+                  '.bmp',
+                  '.bz2',
+                  '.crx',
+                  '.dia',
+                  '.msi',
+                  '.gif',
+                  '.ico',
+                  '.jpg',
+                  '.jpeg',
+                  '.mp4',
+                  '.pdf',
+                  '.png',
+                  '.PNG',
+                  '.swf',
+                  '.tar.gz',
+                  '.tiff',
+                  '.webp',
+                  '.xcf',
+                  '.xlsx',
+                  '.zip'
+                  ]
 
 
 def CheckPatchFormatted(input_api, output_api):
@@ -21,3 +56,42 @@ def CheckPatchFormatted(input_api, output_api):
 def CheckChangeHasDescription(input_api, output_api):
   return input_api.canned_checks.CheckChangeHasDescription(
       input_api, output_api)
+
+
+def CheckForLobs(input_api, output_api):
+  output_status = []
+  for file in input_api.change.AffectedFiles():
+    # The tar.gz for example prevents using a hashmap to look up the extension
+    for ext in LOB_EXTENSIONS:
+      if str(file).endswith(ext) and file.Action() != 'D':
+        error_msg = ('The file \'{file_name}\' is a binary that has not been '
+          'uploaded to GCE. Please run:\n\tscripts/upload_lobs.py '
+          '"{file_name}"\nand commit {file_name}.sha1 instead\n'
+          'Run:\n\tgit rm --cached "{file_name}"\nto remove the lob from git'
+          .format(file_name = file.LocalPath()))
+
+        error = output_api.PresubmitError(error_msg)
+        output_status.append(error)
+        break
+
+  return output_status
+
+
+def CheckLobIgnores(input_api, output_api):
+  output_status = []
+  with open("site/.gitignore", 'r') as ignore_file:
+    ignored_lobs = list(line.rstrip() for line in ignore_file.readlines())
+    ignored_lobs = set(ignored_lobs[
+      ignored_lobs.index('#start_lob_ignore') + 1 :
+      ignored_lobs.index('#end_lob_ignore')])
+
+    for ignored_lob in ignored_lobs:
+      lob_sha_file = os.path.join('site', ignored_lob + '.sha1')
+      if not lob_sha_file.startswith('#') and not os.path.exists(lob_sha_file):
+        error_msg = ('The sha1 file \'{removed_file}\' no longer exists, '
+          'please remove "{ignored_file}" from site/.gitignore'
+          .format(removed_file = lob_sha_file, ignored_file = ignored_lob))
+
+        error = output_api.PresubmitError(error_msg)
+        output_status.append(error)
+  return output_status
