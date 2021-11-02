5e4f0724e6fae22715c940e035d586bc8e9b5ee5
Dirk Pranke <dpranke@google.com>
2021-11-02 14:55:55 -0700

    Add DEPS, eleventy, and firebase configs.
    This adds the DEPS entries needed to pull depot_tools in
    plus the Node, Eleventy, and Firebase configs that we'll
    use. These are all copied over from the development repo
    in experimental/website.
    
    Bug: 1260171
    Change-Id: I26a5a4eb33f8caf79e756c908f38d6d99739d6ab
    Reviewed-on: https://chromium-review.googlesource.com/c/website/+/3258089
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Struan Shrimpton <sshrimp@google.com>

---
+++ .eleventy.js
@@ -0,0 +1,65 @@
+module.exports = config => {
+  config.addWatchTarget('./site/_stylesheets/');
+  config.setTemplateFormats(['md']);
+
+  // Copy binary assets over to the dist/ directory.
+
+  // This list must be kept in sync with the lists in //.eleventy.js and
+  // //scripts/upload_lobs.py.
+  // TODO(dpranke): Figure out how to share these lists to eliminate the
+  // duplication and need to keep them in sync.
+  let lob_extensions = [
+    '.ai',
+    '.bin',
+    '.bmp',
+    '.bz2',
+    '.crx',
+    '.dia',
+    '.msi',
+    '.gif',
+    '.ico',
+    '.jpg',
+    '.jpeg',
+    '.mp4',
+    '.pdf',
+    '.png',
+    '.PNG',
+    '.swf',
+    '.tar.gz',
+    '.tiff',
+    '.webp',
+    '.xcf',
+    '.xlsx',
+    '.zip',
+  ];
+
+  // This should basically pick up everything that isn't a .md file
+  // or a .sha1.
+  // TODO(dpranke): Figure out how to actually enforce this and get
+  // rid of the "basically". There has to be a better approach. :).
+  let extensions = lob_extensions.concat([
+    '.cpp',
+    '.csv',
+    '.dot',
+    '.ebuild',
+    '.el',
+    '.graffle',
+    '.html',
+    'patch',
+    '.py',
+    '.svg',
+    '.txt',
+    '.xml'
+  ]);
+
+  for (let ext of extensions) {
+    config.addPassthroughCopy('site/**/*' + ext);
+  }
+
+  return {
+    dir: {
+      input: 'site',
+      output: 'build'
+    },
+  };
+};
